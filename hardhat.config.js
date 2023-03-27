f9df6f9b36f42928432faa464da0c56550e2962b
Bartek <s210869@sggw.edu.pl>
2023-03-27 16:00:58 +0200

    added sepolia testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,7 @@ require("solidity-coverage");
 require("dotenv").config();
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const PRIVATE_KEY1 = process.env.PRIVATE_KEY1;
 const PRIVATE_KEY2 = process.env.PRIVATE_KEY2;
@@ -22,6 +23,12 @@ module.exports = {
             chainId: 5,
             blockConfirmations: 6,
         },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY1, PRIVATE_KEY2],
+            chainId: 11155111,
+            blockConfirmations: 6,
+        },
         localhost: {
             chainId: 31337,
             blockConfirmations: 1,
@@ -30,6 +37,7 @@ module.exports = {
     etherscan: {
         apiKey: {
             goerli: ETHERSCAN_API_KEY,
+            sepolia: ETHERSCAN_API_KEY,
         },
     },
     solidity: "0.8.17",
